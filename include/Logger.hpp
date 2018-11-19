a8ec439ec44484f019935426f04bf9ce3a7c03e1
Aisu Wata <aisu.wata0@gmail.com>
2018-11-19 11:03:51 -0200

    LogLine update, now uses compile time checking

--- include/Logger.hpp
+++ include/Logger.hpp
@@ -9,22 +9,13 @@
 #include <ios>
 #include <algorithm>
 
-#define LogM(logLine_, LogLvl_, Message_) \
-logLine_.setMsgLvl(LogLvl_); \
-logLine_ << Message_; logLine_.flush();
-
-#ifdef NDEBUG
-	#define LogDEBUG(logLine_, Message_) {}; // do nothing
-#else
-	#define LogDEBUG(logLine_, Message_) LogM(logLine_, LogLvl::Debug, Message_);
-#endif
-
 namespace gm
 {
 
 //!!! Subtract extra lines from LogLvlSTART_LINE if an entry takes more than one
 constexpr auto LogLvlSTART_LINE = __LINE__;
-enum class LogLvl { // Each line has to be an enum, se above
+enum class LogLvl
+{ // Each line has to be an enum, se above
 	Fatal,
 	Critical,
 	Error,
@@ -35,10 +26,15 @@ enum class LogLvl { // Each line has to be an enum, se above
 };
 constexpr auto LogLvlMax = __LINE__ - LogLvlSTART_LINE - 4;
 
-std::ostream& operator<<(std::ostream& out, const LogLvl value){
+std::ostream &operator<<(std::ostream &out, const LogLvl value)
+{
 	std::string s;
-#define CASE_VAL(p) case(p): s = #p; break;
-	switch(value){
+#define CASE_VAL(p) \
+	case (p):        \
+		s = #p;       \
+		break;
+	switch (value)
+	{
 		CASE_VAL(LogLvl::Fatal);
 		CASE_VAL(LogLvl::Critical);
 		CASE_VAL(LogLvl::Error);
@@ -56,87 +52,75 @@ std::ostream& operator<<(std::ostream& out, const LogLvl value){
  * LogLine is used to filter messages by levels, using gm::LogLvl enum
  *
  * example
- * ```cpp
-   gm::LogLine<std::cout> myLine(gm::LogLvl::Info);
-	// myLine will only log messages with LogLvl of tier Info or higher like Warn
+   ```cpp
+    gm::LogLine<gm::LogLvl::Warn> myLine(std::cout);
+	// myLine will only log messages with Lvl Warn or higher like Fatal because of the constructor
 
-	// "setMsgLvl()" changes LogLvl of this and subsequent messages
-  	myLine.setMsgLvl(gm::LogLvl::Info)
-  	<< "Information message" << std::endl;
+	// "msg()" sets LogLvl of this and subsequent messages
+  	myLine.msg(gm::LogLvl::Info) << "Information message" << std::endl;
+	// so this wont be logged, myLine is on Warn Lvl
 
-	myLine << "this message also has LogLvl::Info" << std::endl;
-
-	// "setMsgLvl()" changed LogLvl subsequent messages to Warn
-  	myLine.setMsgLvl(gm::LogLvl::Warn);
+	// "msg()" changed LogLvl subsequent messages to Warn
+  	myLine.msg(gm::LogLvl::Warn);
 	myLine << "this message has LogLvl::Warn" << std::endl;
-
-	// "msg()" sends a message of LogLvl given
-  	myLine.msg(gm::LogLvl::Note) << "one time Note without changing Line Msg LogLvl" << std::endl;
-
-	// myLine will only log messages with LogLvl of tier Critical or higher like Fatal
-	myLine.setLvl(LogLvl::Critical);
-
-	//  setMsgLvl was set to Warn beforehand by setMsgLvl()
-	myLine << "another message that has LogLvl::Warn" << std::endl;
-	// so this wont be logged
-
- * ```
- * @param out   [description]
- * @param level [description]
+	
+   // The following bad practice in isolation since you might have to find
+	//  where the msg Lvl was last set by msg()
+	myLine << "a message which I don't know immediatly what level it is" << std::endl;
+   ```
+ * @tparam logLvl_ Lvl of the LogLine
+ * @param out   ostream to output logs
  */
-class LogLine {
-public:
-  LogLine(std::ostream &outStream, LogLvl level = LogLvl::Warn)
-		: outStream_(outStream), logLvl_(level), setMsgLvl_(level)
+template <LogLvl logLvl_>
+class LogLine
+{
+ public:
+	LogLine(std::ostream &outStream)
+		 : outStream_(outStream),
+			// logLvl_(level),
+			setMsgLvl_(logLvl_)
 	{
 	}
 
-	void setLvl(LogLvl level){ logLvl_ = level; }
-
 	void flush() { outStream_ << std::flush; }
 
-	LogLine& setMsgLvl(LogLvl level){
+	LogLine &msg(const LogLvl &level){
 		setMsgLvl_ = level;
 		return *this;
 	}
 
-	std::ostream& msg(LogLvl level){
-		if(level <= logLvl_){
-			return outStream_;
-		} else {
-			return nullStream_;
-		}
-	}
-
-	template<class T>
-	std::ostream& operator<<(const T& thing) {
-		if(setMsgLvl_ <= logLvl_){
+	template <class T>
+	LogLine &operator<<(const T &thing){
+		if (setMsgLvl_ <= logLvl_){
 			outStream_ << thing;
-			return outStream_;
-		} else {
-			return nullStream_;
+			return *this;
+		}
+		else{
+			return *this;
 		}
 	}
 	// logger << std::endl;
-	std::ostream& operator<<(std::ostream& (*f)(std::ostream&)) {
-		if(setMsgLvl_ <= logLvl_) {
+	LogLine &operator<<(std::ostream &(*f)(std::ostream &)){
+		if (setMsgLvl_ <= logLvl_){
 			f(outStream_);
-			return outStream_;
-		} else {
-			return nullStream_;
+			return *this;
+		}
+		else{
+			return *this;
 		}
 	}
 
-
-//private:
+private:
 	std::ostream &outStream_;
-	std::ofstream nullStream_;
-	LogLvl logLvl_;
+	// const LogLvl logLvl_;
 	LogLvl setMsgLvl_;
 	//static LogFilter...
 };
 
-extern LogLine logger;
-LogLine logger(std::clog, LogLvl::Warn);
+// // example declaration
+// // in a common header between all files that use the line:
+// extern gm::LogLine<gm::LogLvl::Warn> logger;
+// // in main.cpp:
+// gm::LogLine<gm::LogLvl::Warn> logger(std::clog);
 
-}
+} // namespace gm
